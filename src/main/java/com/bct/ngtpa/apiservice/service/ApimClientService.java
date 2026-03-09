4d0572b6252272f31de2bb481786b3ca3e1635d2
Wang kwan Ching <kcw30165@gmail.com>
2026-03-09 15:54:21 +0800

    update logging

--- src/main/java/com/bct/ngtpa/apiservice/service/ApimClientService.java
+++ src/main/java/com/bct/ngtpa/apiservice/service/ApimClientService.java
@@ -4,20 +4,25 @@ import static org.springframework.security.oauth2.client.web.reactive.function.c
 
 import com.bct.ngtpa.apiservice.dto.apim.AccountBalanceRequest;
 import com.bct.ngtpa.apiservice.dto.apim.AccountBalanceResponse;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 public class ApimClientService {
     private static final String ACCOUNT_BALANCE_API_NAME = "account-balance";
-
+    private final ObjectMapper objectMapper;
     private final WebClient apimWebClient;
     private final ApimPayloadCryptoService apimPayloadCryptoService;
 
-    public ApimClientService(WebClient apimWebClient, ApimPayloadCryptoService apimPayloadCryptoService) {
+    public ApimClientService(WebClient apimWebClient, ApimPayloadCryptoService apimPayloadCryptoService, ObjectMapper objectMapper) {
         this.apimWebClient = apimWebClient;
         this.apimPayloadCryptoService = apimPayloadCryptoService;
+        this.objectMapper = objectMapper;
     }
 
     public Mono<AccountBalanceResponse> getAccountBalance(AccountBalanceRequest accountBalanceRequest) {
@@ -25,6 +30,12 @@ public class ApimClientService {
                 ACCOUNT_BALANCE_API_NAME,
                 accountBalanceRequest,
                 AccountBalanceRequest.class);
+                try {
+                    log.info("APIM request body={}", objectMapper.writeValueAsString(encryptedRequest));
+                } catch (Exception e) {
+                    log.error("Failed to convert request body to JSON", e);
+                }
+
 
         return apimWebClient.post()
                 .uri(uriBuilder -> uriBuilder.path("TRPGetEEBal").build())
