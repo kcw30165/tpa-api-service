ab4adf465da04d817fc741ce69d39922efa309d7
Nick Deng <deng.nick@bcthk.com>
2026-06-18 18:38:22 +0800

    modify session-id name

--- src/main/java/com/bct/ngtpa/apiservice/shared/web/RequestHeaderContextKeys.java
+++ src/main/java/com/bct/ngtpa/apiservice/shared/web/RequestHeaderContextKeys.java
@@ -5,7 +5,7 @@ public final class RequestHeaderContextKeys {
     public static final String ACCOUNT_REF_HEADER = "Account-Ref";
     public static final String ACCEPT_LANGUAGE_HEADER = "Accept-Language";
     
-    public static final String SESSION_ID_HEADER = "sid_xxx";
+    public static final String SESSION_ID_HEADER = "Session-Id";
 public static final String ATTRIBUTE_KEY = RequestHeaderContext.class.getName() + ".attribute";
     public static final String CONTEXT_KEY = RequestHeaderContext.class.getName() + ".context";
 
